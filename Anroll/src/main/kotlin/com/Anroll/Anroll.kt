d49db81e3e603041208ee207205d1d1cc8032514
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-09 20:30:17 -0300

    Update Anroll.kt

--- Anroll/src/main/kotlin/com/Anroll/Anroll.kt
+++ Anroll/src/main/kotlin/com/Anroll/Anroll.kt
@@ -26,7 +26,7 @@ class Anroll : MainAPI() {
     "adicionados" to "Animes em Alta",
     "filmes" to "Filmes"
 )
-override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+ override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(mainUrl).document
         val scriptTag = document.selectFirst("script#__NEXT_DATA__")
             ?: return newHomePageResponse(request.name, emptyList())
@@ -53,7 +53,7 @@ override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageR
                 val posterUrl = entry?.optString("poster") ?: entry?.optString("capa_filme")
                 val url = "$mainUrl/a/${entry?.optString("generate_id")}"
                 val type = if (request.data == "filmes") TvType.Movie else TvType.Anime
-
+                
                 items.add(
                     newSearchResponse(title, url, type) {
                         this.posterUrl = posterUrl
@@ -61,7 +61,7 @@ override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageR
                 )
             }
         }
-
+        
         return newHomePageResponse(
             list = HomePageList(
                 name = request.name,
