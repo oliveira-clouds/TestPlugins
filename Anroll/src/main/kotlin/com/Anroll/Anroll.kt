53bfe3678cb793de6e791d3b587e05d1ee8db3ca
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-08 22:04:20 -0300

    Update Anroll.kt

--- Anroll/src/main/kotlin/com/Anroll/Anroll.kt
+++ Anroll/src/main/kotlin/com/Anroll/Anroll.kt
@@ -165,7 +165,7 @@ class Anroll : MainAPI() {
                         "Anroll",
                         "Anroll",
                         fixUrl(videoUrl),
-                        mainUrl
+                        mainUrl,
                     )
                 )
                 return true
