74fa5dc10c9b0910657bbb92f8391abd67be8c52
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-16 00:11:38 -0300

    primeira versão estável completa

--- Anroll/build.gradle.kts
+++ Anroll/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     description = "Anroll - Animes Legendados e Dublados"
     language = "pt-br"
     authors = listOf("Sephiroth") 
-    status = 2 
+    status = 1 
     tvTypes = listOf("Anime") 
     iconUrl = "https://www.anroll.net/_next/image?url=https%3A%2F%2Fstatic.anroll.net%2Fapp-images%2Ffavicon.ico&w=32&q=75"
     isCrossPlatform = true
