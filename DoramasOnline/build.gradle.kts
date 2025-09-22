63246f8ef23548e520215891c6d1e2d6378e4251
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-22 13:39:49 -0300

    Create build.gradle.kts

---
+++ DoramasOnline/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Doramas Online - Filmes e Séries Legendados e Dublados"
+    language = "pt-br"
+    authors = listOf("Sephiroth")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://doramasonline.org/wp-content/uploads/2022/04/pngegg.png"
+    isCrossPlatform = true
+}
