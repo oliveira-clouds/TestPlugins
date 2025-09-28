3ecc6acff083a17c7637869b70fed86b95b37b23
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-28 17:08:36 -0300

    Update Plugin.kt

--- AnimeQ/src/main/kotlin/com/AnimeQ/Plugin.kt
+++ AnimeQ/src/main/kotlin/com/AnimeQ/Plugin.kt
@@ -1,4 +1,4 @@
-package com.lagradost.animeq
+package com.animeq
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
