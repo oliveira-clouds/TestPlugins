cb1f588fbb5b78d461671e7c846ca794f3c1a9f9
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-09-28 17:02:04 -0300

    Update build.gradle.kts

--- AnimeQ/build.gradle.kts
+++ AnimeQ/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     description = "AnimeQ HD e FHD"
     language = "pt-br"
     authors = listOf("Sephiroth") 
-    status = 1 
+    status = 2 
     tvTypes = listOf("Anime") 
     iconUrl = "https://animeq.blog/wp-content/uploads/2025/06/Favicon-AnimeQ-1.png"
     isCrossPlatform = true
