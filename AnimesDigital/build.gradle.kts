80a1f5bedabd4a5951429f3465f2865ae344bc42
oliveira-clouds <oliveirainscricoes@gmail.com>
2025-10-10 14:46:23 -0300

    Update build.gradle.kts

--- AnimesDigital/build.gradle.kts
+++ AnimesDigital/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     description = "Animes Digital - Animes Legendados, dublados, filmes e desenhos "
     language = "pt-br"
     authors = listOf("Sephiroth") 
-    status = 2 
+    status = 1
     tvTypes = listOf("Anime") 
     iconUrl = "https://animesdigital.org/wp-content/uploads/2025/03/cropped-logo-e1740827992823-192x192.png"
     isCrossPlatform = true
